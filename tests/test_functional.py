fabc7e148bb56d07748ae3df5a2f6098d6660cd1
Aaron Diez <aronpes94@gmail.com>
2018-05-08 12:46:05 +0200

    functional front test added

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -2,19 +2,38 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    def test_search_in_python_org(self):
+    def test(self):
         driver = self.driver
-        driver.get("localhost:8000")
-        
+        driver.get("http://localhost:8000")
+        self.login()
+        self.createProject()
+        self.logout()
 
+    def login(self):
+        self.driver.find_element_by_id('id_username').send_keys('admin')
+        self.driver.find_element_by_id('id_password').send_keys('adminadmin')
+        self.driver.find_element_by_id('btn-login').click()
+
+    def createProject(self):
+        self.driver.find_element_by_id('newProject').click()
+        element = self.driver.find_element_by_id('id_user')
+        all_options = element.find_elements_by_tag_name("option")
+        for option in all_options:
+            option.click()
+        self.driver.find_element_by_id(
+            'id_project_name').send_keys('this is a project name')
+        self.driver.find_element_by_id(
+            'id_description').send_keys('this is a description')
+        self.driver.find_element_by_id('btn-save').click()
+
+    def logout(self):
+        self.driver.find_element_by_id('btnLogout').click()
 
     def tearDown(self):
         self.driver.close()
-
-if __name__ == "__main__":
-    unittest.main()
